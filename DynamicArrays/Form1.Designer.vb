7fc73ae088af4b54dff49af7e8a443443c308e01
collins njoka <karimicnjoka@gmail.com>
2018-02-13 12:55:17 +0300

    new files

--- DynamicArrays/Form1.Designer.vb
+++ DynamicArrays/Form1.Designer.vb
@@ -22,9 +22,40 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 21)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(263, 290)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(320, 42)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(459, 362)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button1 As Button
 End Class
--- DynamicArrays/Form1.vb
+++ DynamicArrays/Form1.vb
@@ -1,3 +1,12 @@
 ﻿Public Class Form1
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sqr() As Integer
+        Dim i, n As Integer
+        n = InputBox("Enter the size of array")
+        ListBox1.Items.Clear()
+        For i = 0 To n
+            ReDim sqr(i)
+            ListBox1.Items.Add(sqr(i))
+        Next
+    End Sub
 End Class
